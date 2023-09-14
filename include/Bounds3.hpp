08b97d03c86bc8be80fc6463156c9d8acdad4365
peizhao <s2160679@ed.ac.uk>
2023-09-14 02:35:54 +0100

    understand bound box code

--- include/Bounds3.hpp
+++ include/Bounds3.hpp
@@ -59,7 +59,7 @@ class Bounds3
             return (x && y && z);
         }
 
-        bool Inside(const Vec3& p, const Bounds3& b)
+        bool Inside(const Vec3f& p, const Bounds3& b)
         {
             return (p.x >= b.pMin.x && p.x <= b.pMax.x && p.y >= b.pMin.y &&
                     p.y <= b.pMax.y && p.z >= b.pMin.z && p.z <= b.pMax.z);
